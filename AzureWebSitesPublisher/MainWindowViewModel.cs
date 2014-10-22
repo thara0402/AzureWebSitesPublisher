6697371b986748521c780596df44eafd8d410a1d
thara0402 <thara0402@gmail.com>
2014-10-22 17:35:38 +0900

    暫定実�

---
+++ AzureWebSitesPublisher/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureWebSitesPublisher
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _publishSettingsPath;
+        private string _sourcePath;
+
+        public string PublishSettingsPath
+        {
+            get { return this._publishSettingsPath; }
+            set { this.SetProperty(ref this._publishSettingsPath, value); }
+        }
+
+        public string SourcePath
+        {
+            get { return this._sourcePath; }
+            set { this.SetProperty(ref this._sourcePath, value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+    }
+}
